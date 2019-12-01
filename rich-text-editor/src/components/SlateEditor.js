c8682312514348973c035f795eaf96ecabf5edf5
angel-torres <alas4224@gmail.com>
2019-12-01 01:08:07 -0600

    added a renderElement function to SlateEditor component

--- rich-text-editor/src/components/SlateEditor.js
+++ rich-text-editor/src/components/SlateEditor.js
@@ -2,6 +2,8 @@ import React, { useMemo } from 'react';
 import { withReact, Slate, Editable } from 'slate-react'
 import { createEditor } from 'slate';
 import initialValue from '../utils/value';
+import CodeElemeng from './elements/CodeElement';
+import DefaultElement from './elements/DefaultElement';
 
 export default function SlateEditor() {
     const editor = useMemo(() => withReact(createEditor()), [])
@@ -10,16 +12,21 @@ export default function SlateEditor() {
             e.preventDefault()
             editor.exec({type:"insert_text", text:"and"})
         }
-        if (e.key === "b") {
-            e.preventDefault();
-            editor.exec({type:"add_mark", mark:{type: "bold"}})
-        }
     } 
+
+    const renderElement = useCallback((props) => {
+        switch (props.element.type) {
+            case 'code':
+                return <CodeElement {...props} />
+            default:
+                return <DefaultElement {...props} />
+        }
+        },[])
 
     return (
         <div>
             <h1>The SlateEditor is supposed to go here!</h1>
-            <Slate editor={editor} defaultValue={initialValue}>            
+            <Slate editor={editor} renderElement={renderElement} defaultValue={initialValue}>            
                 <Editable onKeyDown={keyDown}/>
             </Slate>
         </div>
---
+++ rich-text-editor/src/components/elements/DefaultElement.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DefaultElement(props) {
+    return (
+        <p {...props.attributes}>
+           {props.children} 
+        </p>
+    )
+}
